15944189ba639a04903958beb60727917ad979e2
Gali Deutsch <deutsch.gali@gmail.com>
2021-06-14 13:16:49 -0400

    initial setup

---
+++ client/src/components/App/App.js
@@ -0,0 +1,17 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+      
+      </header> */}
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
