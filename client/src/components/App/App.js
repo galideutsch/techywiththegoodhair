5b30daa41c9dc89f8bd0dca06232f687ca4dc422
Gali Deutsch <deutsch.gali@gmail.com>
2021-07-15 16:35:06 -0400

    setting up dictionary fetch

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,15 +1,49 @@
-
 import './App.scss';
+import { useEffect, useState } from 'react';
+import { usePrevious} from '../../infra/Hooks';
+import axios from 'axios';
 
-function App() {
+const url_path = "http://techywiththegoodhair.com/en_us";
+
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [dictionary, setDictionary] = useState([]);
+  const getData = async () => {
+    const data = await axios.get(url_path)
+    .then((response) => {
+      setDictionary(response);
+    })
+    .catch((err) => {
+      setError('ERROR: fetch failed');
+      
+    });
+    return data;
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    getData().then(() => setLoading(false));
+  }, []); 
+
+  if (loading) {
+    return (<div className="loading">Loading...</div>)
+  }
+
+  if (error) {
+    return (<div className="error">{error}</div>)
+  }
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-      
-      </header> */}
-      <div>
-        
-      </div>
+      {dictionary.data && (
+        <div>
+          <header className="App-header">
+            {dictionary.data.TechyWithTheGoodHairTitle}
+          </header>
+        </div>
+      )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
 const publicPath = path.join(__dirname, '../client', 'public');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
+
+const en_us = require('./languages/en_us.json');
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+app.get('/en_us', (req, res) => {
+   res.send(en_us);
+ });
 
 app.listen(port, () => {
    console.log('Server is up!');
